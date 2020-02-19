34277db2c2a427717b9b967c02e11727d7704478
Veronica <elyrua122@gmail.com>
2020-02-19 10:13:41 -0500

    RESTServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,18 @@
 // Puerto
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//Entorno 
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';    
+// } else {
+//     urlDB = 'mongodb + srv : // Ana : sTycQpVu1rXJKUD4 @ cluster0-gnfja.mongodb.net / cafe ? retryWrites = true & w = mayority'
+// }
+
+process.env.URLDB = urlDB;
